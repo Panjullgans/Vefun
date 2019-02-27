062a5ca7a484c671719056b1d4ee65af88ffa4b0
Panjull Gans <48045740+Panjullgans@users.noreply.github.com>
2019-02-27 14:17:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$config = [
+"ticket"=>"sHp2r312otmCpnGns3mCqJhovNiBin-Vu72Br7-4f8q9fYOif4bMk4-2iWXAm3mpkLC7lYqgh96vqomwv9uUl62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ",
+"meid"=>"7513e93c2c9cf6c2"
+];
